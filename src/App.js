c0bfca3b313868027f204bce332d6ec15298a606
Robert Henderson <henderson@Roberts-MacBook-Pro-2.local>
2019-08-30 19:23:28 +0200

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Hats = () => {
+  return <div>Hats</div>;
+};
+const Topics = () => {
+  return <div>Topics</div>;
+};
+const Details = () => {
+  return <div>Details</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/Hats" component={Hats} />
+        <Route path="/Topics" component={Topics} />
+        <Route path="/Details" component={Details} />
+      </BrowserRouter>
     </div>
   );
 }
